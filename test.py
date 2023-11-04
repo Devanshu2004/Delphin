821b0497b6a4fe60ac6a299b4a79b3e3b372159a
Devanshu Joshi <92581265+Devanshu2004@users.noreply.github.com>
2023-11-04 11:28:27 +0530

    test.py

--- test.py
+++ test.py
@@ -2,33 +2,36 @@ import csv
 import numpy as np
 import open3d as o3d
 
+# List of distances from the sensor 
 distances = []
 
 with open("TestData.csv", "r") as test_data:
     reader = csv.reader(test_data)
+    
+    # Removing redundances from the input csv file
     for line in reader:
         row = line[0]
         if " cm" in row:
             row = row.replace(" cm", '')
         distances.append(row)
 
-for distance in distances:
-    distance = float(distance)
+print(type(distances[2]))
 
-x_axis[len(distances)]
+# Conversion from string into float
+distances = [float(x) for x in distances]
+
+print(type(distances[2]))
+
+
+x_axis = distances
+y_axis = [x + 24.32 for x in distances]
+z_axis = [x - 90.84 for x in distances]
 
 print(distances)
-for i in range(len(distances)):
-    x_axis[i] = distances[i]
-
-for i in range(len(distances)):
-    y_axis[i] = distances[i]
-
-for i in range(len(distances)):
-    z_axis[i] = distances[i]
 
 point_data = np.stack([x_axis, y_axis, z_axis], axis=0).transpose((1, 0))
 
+# Plotting
 geom = o3d.geometry.PointCloud()
 geom.points = o3d.utility.Vector3dVector(point_data)
 o3d.visualization.draw_geometries([geom])
